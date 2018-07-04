60563d1c68b5bc54df40a77e01c44ffd6ffd848c
Ugesh Malempati <malempati99@gmail.com>
2018-07-04 19:37:31 +0000

    API for Sensor Data

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 /* **************** Importing external and internal required dependenices and files ******************** */
-const express                     = require('express'),
-      app                         = express(),
-      bodyParser                  = require('body-parser'),
-      http                        = require('http'),
-      socketIO                    = require('socket.io'),
-      path                        = require('path'),
-      getDhtSensorReadings        = require('./models/getDhtReadings');
+const express         = require('express'),
+      app             = express(),
+      bodyParser      = require('body-parser'),
+      http            = require('http'),
+      socketIO        = require('socket.io'),
+      path            = require('path'),
+      sensorCacheData = require('./models/getCachedSesnorReadings.js'),
+      sensorDb        = require('./models/sensor_db');
 
 /* **************** APP Config ****************** */
 app.set('view engine', 'ejs');
@@ -13,48 +14,63 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'client')));
 
-/* ************ Temporary storage for sensor readings *********** */
-const cache = {
-     temperature: null,
-     humidity: null
-};
 
 app.get('/', function (req, res) {
-     res.render('sensorreadings');
-});
+         res.render('sensorreadings');
+    });
+
+/* **************** Fetching last ten temperature readings ******************** */ 
+app.get('/temperature/histroy', (err, res) => {
+         
+         if (err) {
+                 console.log(`Temperature Fetching Error: ${err}`); 
+            } else {
+                 sensorDb.fetchLatestReadings('temperature', 10, (err, data) => {
+                     if (err) {
+                             console.log(err);
+                             return res.status(500).end();
+                        } else {
+                             res.json(data.reverse()); 
+                        }   
+                    });   
+                }
+    });
+
+/* **************** Fetching last ten humidity readings ******************** */ 
+app.get('/humidity/histroy', (err, res) => {
+         
+        if (err) {
+             console.log(`Humidity Fetching Error: ${err}`); 
+            } else {
+                 sensorDb.fetchLatestReadings('humidity', 10, (err, data) => {
+                     if (err) {
+                             console.log(err);
+                             return res.status(500).end();
+                        } else {
+                             res.json(data.reverse()); 
+                        }   
+                    });   
+                }
+    });    
+
 /* ************ Creatig HTTP Server ************ */
 const httpServer = http.createServer(app);
 /* ************* Integrating server with socket *************** */
 const serverSocket = socketIO(httpServer);
 serverSocket.on('connection', (socket) => {
-     console.log('Client has connected');
-     //Function to read sensor readings every 2 seconds
-     setInterval(() => {
-         getDhtSensorReadings((err, DHTTemp, DHTHumi) => {
-             if (err) {
-                 return console.log(`DHT Sensor Reading: ${err}`);
-                } else {
-                         if(cache.temperature !== DHTTemp) {
-                             //attaching cutom event for new temperature value
-                              socket.emit('newTemparature', {
-                                     temperature: DHTTemp 
-                                });   
-                            }
-                         if(cache.humidity !== DHTHumi){
-                             //attaching cutom event for new humidity value
-                              socket.emit('newHumidity', {
-                                     humidity: DHTHumi 
-                                });
-                            }
-                    }
+         console.log('Client has connected');
+         socket.emit('newTemparature', {
+                 temperature: sensorCacheData.getTemperature 
+            });   
+         socket.emit('newHumidity', {
+                 humidity: sensorCacheData.getHumidity 
             });
-        }, 2000);
         
-     //Listening for client disconnection
-     socket.on('disconnect', () => {
-          console.log('Client has disconnected'); 
-     });
-});
+         //Listening for client disconnection
+         socket.on('disconnect', () => {
+                 console.log('Client has disconnected'); 
+            });
+    });
 
 httpServer.listen(3030, () => {
      console.log('IoT Dashboard has started'); 
